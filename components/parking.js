c01d4abc963b59c3e68cc8af3b20918503974512
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-03-11 09:09:33 -0800

    add

--- components/parking.js
+++ components/parking.js
@@ -1,6 +1,10 @@
 import { Text, StyleSheet, View, ScrollView, Dimensions, TouchableWithoutFeedback, Image } from 'react-native'
 import React, { Component, useState } from 'react'
 import Items from '../json/parkings.json'
+import Tag from '../images/tag.png'
+import Rate from '../images/star.png'
+import {Picker} from '@react-native-picker/picker';
+
 
 const {width, height} = Dimensions.get('screen')
 
@@ -25,19 +29,46 @@ export default function Parking() {
 
             <View>
              <Text  key={index}>{x.title}</Text>
+             <Text
+                style={{
+                    borderWidth:1,
+                    padding:2,
+                    borderColor:'grey',
+                    borderRadius:5,
+                    color:'grey',
+                    fontWeight:'500',
+                    marginTop:5,
+                }}
+             >05:00 hrs</Text>
+            <Picker
+  selectedValue={hours[x.id]}
+  onValueChange={(itemValue, itemIndex) =>
+    setHours({...hours, [x.id]:itemIndex})
+  }>
+  <Picker.Item label="1:00" value="1" />
+  <Picker.Item label="2:00" value="2" />
+  <Picker.Item label="3:00" value="3" />
+  <Picker.Item label="4:00" value="4" />
+  <Picker.Item label="5:00" value="5" />
+  <Picker.Item label="6:00" value="6" />
+</Picker>
             </View>
 
         <View style={{flex:1, flexDirection:'row'}}>
-            <View style={{flex:1, flexDirection:'column'}}>
-                <Text>${x.price}</Text>
-                <Text>  {x.rating}</Text>
+            <View style={{flex:1, flexDirection:'column', marginLeft:40, marginVertical:15}}>
+                <Text style={{fontWeight:'bold', fontSize:12,marginBottom:10}}>
+                <Image source={Tag} style={{width:15, height:15,marginRight:10}}/>
+                    ${x.price}</Text>
+                <Text style={{fontWeight:'bold', fontSize:12}}> <Image source={Rate} style={{width:15, height:15,marginLeft:10}}/> {x.rating}</Text>
             </View>
 
             <TouchableWithoutFeedback style={{flex:1}} >
 
                     <View style={{backgroundColor:'coral', flexDirection:'row', justifyContent:'space-between', padding:10, borderRadius:6}}>
                         <View >
-                                <Text style={{fontWeight:'bold', fontSize:20, color:'white'}}>${x.price * 2}</Text>
+                                <Text style={{fontWeight:'bold', fontSize:20, color:'white'}}>
+                                   
+                                    ${x.price * 2}</Text>
                                 <Text style={{color:'white', fontSize:10}}>{x.price}x{hours[x.id]} hrs</Text>
                          </View>
 
